070a393d0ac6d3098a14e787334fd341ada1b5fc
Prayesh13 <prayeshgodhani04@gmail.com>
2025-04-15 11:22:47 +0530

    add docker workflow

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,11 +4,73 @@ from mlflow.tracking import MlflowClient
 import pandas as pd
 import pickle
 import os
-import sys
+import numpy as np
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-# Add the parent directory to sys.path so Python can locate preprocessing_utility
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from flask_app.preprocessing_utility import normalize_text
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
+
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
+
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
+
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
+
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub('\s+', ' ', text).strip()
+    return text
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def remove_small_sentences(df):
+    """Remove sentences with less than 3 words."""
+    for i in range(len(df)):
+        if len(df.text.iloc[i].split()) < 3:
+            df.text.iloc[i] = np.nan
+
+def normalize_text(text):
+    """Normalize the text data."""
+    try:
+        text = lower_case(text)
+        text = remove_stop_words(text)
+        text = removing_numbers(text)
+        text = removing_punctuations(text)
+        text = removing_urls(text)
+        text = lemmatization(text)
+        
+        return text
+    except Exception as e:
+        print(f"Error in normalizing text: {e}")
+        raise
 
 def get_latest_model_version(model_name: str) -> int:
     """
@@ -86,4 +148,4 @@ def predict():
 
 if __name__ == '__main__':
     # Run the Flask app
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0",debug=True, port=5000)
